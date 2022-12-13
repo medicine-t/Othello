0e2fda275bf445cbff62a02007df6adae27a8645
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2022-12-13 10:10:08 +0900

    盤面プリント用関数の用意

--- app/src/main/java/jp/ac/uryukyu/e225717/OthelloBoard.java
+++ app/src/main/java/jp/ac/uryukyu/e225717/OthelloBoard.java
@@ -9,4 +9,20 @@ public class OthelloBoard {
         blackBoard[3][4] = blackBoard[4][3] = true;
     }
 
+    void printBoard() {
+        System.out.println(" 01234567");
+        for (int i = 0; i < 8; i++) {
+            System.out.printf("%d", i);
+            for (int j = 0; j < 8; j++) {
+                if (whiteBoard[i][j]) {
+                    System.out.printf("o");
+                } else if (blackBoard[i][j]) {
+                    System.out.printf("x");
+                } else {
+                    System.out.printf(".");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
